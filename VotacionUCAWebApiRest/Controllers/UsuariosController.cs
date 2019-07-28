5967b115fc9d1df01e306673f50bd3e499d8f7c0
JJ <jj.orozcolopez.13@gmail.com>
2019-07-28 13:15:23 -0600

    Controlador para Usuarios
    Creado el controlador y el método para obtener registros.

---
+++ VotacionUCAWebApiRest/Controllers/UsuariosController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using VotacionUCAWebApiRest.Models;
+
+namespace VotacionUCAWebApiRest.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private VotacionUCA votacionBD = new VotacionUCA();
+        
+        [HttpGet]
+        public IEnumerable<Usuarios> Get()
+        {
+            return votacionBD.Usuarios.ToList();
+        }
+    }
+}
