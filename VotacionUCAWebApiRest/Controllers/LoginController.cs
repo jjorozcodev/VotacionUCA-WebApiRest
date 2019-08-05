5ca37cd735f76b819b307b6229343f8af30c1b7e
JJ <jj.orozcolopez.13@gmail.com>
2019-08-05 10:05:33 -0600

    Método login y actualización de modelo EF
    Se añade método para loguearse.
    
    Se actualiza modelo de base de datos, ahora sin relaciones.

---
+++ VotacionUCAWebApiRest/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VotacionUCAWebApiRest.Models;
+
+namespace VotacionUCAWebApiRest.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private VotacionUCA votacionBD = new VotacionUCA();
+
+        [HttpPost]
+        public IHttpActionResult Login([FromBody]LoginModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuarios us = votacionBD.Usuarios.Where(u => u.Usuario == login.Usuario && u.Clave == login.Clave).FirstOrDefault();
+
+                if (us != null)
+                {
+                    return Ok(us);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                votacionBD.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- VotacionUCAWebApiRest/Models/VotacionBD.Designer.cs
+++ VotacionUCAWebApiRest/Models/VotacionBD.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\JJ\Documents\GitHub\VotacionUCA-WebApiRest\VotacionUCAWebApiRest\Models\VotacionBD.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\JO-G\VotacionUCA-WebApiRest\VotacionUCAWebApiRest\Models\VotacionBD.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
